93407386711b32e9933d1ae2cfa158e50ca91b66
Seth Johnson <samaritanbrotherseth@gmail.com>
2025-08-16 21:00:37 -0400

    added the functionality of the calculator

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const display = document.getElementById("display")
 
+
+
 const append = (value) => {
     display.value += value
 }
 
-append()
+const clearScreen = () => {
+    display.value = ""
+}
+
+const deleteChar = () =>{
+    display.value = display.value.slice(0, -1)
+}
+
+const calculate = () =>{
+    try{
+    display.value = eval(display.value)}
+    catch{
+        console.error('Not a true function')
+        display.value= 'Not a real funcition'
+    }
+}
